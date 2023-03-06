801b162141608c8a1afbe8786594d69079389bb8
dr298 <13935995+dr298@users.noreply.github.com>
2023-03-06 10:12:13 +0700

    Update ig-username-to-name.py

--- ig-username-to-name.py
+++ ig-username-to-name.py
@@ -1,6 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+# Print banner and prompt user to continue
+print(f"""
+         .___      ________  ________  ______  
+       __| _/______\_____  \/   __   \/  __  \ 
+      / __ |\_  __ \/  ____/\____    />      < 
+     / /_/ | |  | \/       \   /    //   --   \\
+     \____ | |__|  \_______ \ /____/ \______  /
+          \/               \/               \/ 
+== good or bad? it depends on your point of view! ==
 
+Follow this step to login to your truecaller account :
+1. On Truecaller Android, tap on the 3 line menu on top left then click on setting's.
+2. Tap on Privacy Center and then click on Download my data.
+3. Now a JSON file is downloaded.
+4. Save the JSON file at this tools file location
+5. Rename the JSON file to truecaller.json
+
+""")
 # Read usernames from file
 with open("username.txt") as f:
     usernames = [line.strip() for line in f]
